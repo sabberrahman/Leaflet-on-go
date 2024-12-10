bc310799f4c01d2a71aa901b40df7c0d75b39eee
sabberrahman <sabbersr1@gmail.com>
2024-12-10 15:20:58 +0600

    leaflet- bangladesh population

--- mapping bd/app.js
+++ mapping bd/app.js
@@ -1,4 +1,5 @@
-
+var allPoints=[];
+var searchWithin;
 
 const map = L.map("map").setView([23.777176, 90.399452], 7);
 
@@ -17,9 +18,49 @@ var mapJSon = L.geoJSON(mapData,{style: feature => ({
 }),onEachFeature:returnOnEach}).addTo(map)
 
 
+const styleOptions = {
+    color: "blue", 
+    weight: 1, 
+    fillColor: "#f2f2f2", 
+    fillOpacity: 0.6
+};
+
+var education = L.geoJSON.ajax("data/education-data.geojson",{onEachFeature:returnOnEdu,style: feature => ({
+    color: "red", // Boundary color
+    weight: 1,
+    fillColor: "blue", // Default fill color
+    fillOpacity: 0.6
+})}).addTo(map)
+
+baseLayer={
+    "OSM":titleLayer 
+}
+
+otherLayers={
+    "bangladesh Dist": mapJSon,
+    "School, collage": education
+}
+
+L.control.layers(baseLayer,otherLayers).addTo(map)
+
 // ************functions***********
 function returnOnEach(feature,layer){
-  //console.log(feature)
+     //console.log(layer._latlngs[0]) 
+     var latlngPoly = layer._latlngs[0].map(latlng => [latlng.lng, latlng.lat]);
+
+     // Construct GeoJSON Polygon
+     var polygonGeoJSON = {
+       type: "Feature",
+       geometry: {
+         type: "Polygon",
+         coordinates: [latlngPoly] // Must be an array of arrays
+       }
+     };
+     
+     // Use the GeoJSON in Turf.js
+     var ptsWithin = turf.pointsWithinPolygon(allPoints, polygonGeoJSON);
+     console.log(ptsWithin);
+
     layer.on({
         mouseover: e => {
             const layer = e.target;
@@ -44,8 +85,8 @@ function returnOnEach(feature,layer){
             center = coordinates[0][0][0];
         }
 
-        console.log(center)
-     //   console.log(coordinates)
+    // console.log(layer)
+     //  console.log(coordinates)
     
         // Fly to the center
         if (center) {
@@ -53,6 +94,7 @@ function returnOnEach(feature,layer){
         } else {
             console.error("Could not determine center for the feature.");
         }
+       
     });
   const details = `
   <strong>${feature.properties.shapeName}</strong><br>
@@ -61,3 +103,9 @@ function returnOnEach(feature,layer){
 layer.bindTooltip(details, { sticky: true });
 }
 
+
+function returnOnEdu(feature,layer){
+ // console.log(layer._latlng)
+  allPoints.push(layer._latlng)
+}
+console.log(allPoints)
